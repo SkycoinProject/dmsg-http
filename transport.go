9fc102241d8d1ae675b1aad3710b8803df4afddc
Srdjan Radulovic <srdjan@blue-chang.com>
2020-01-27 14:28:12 +0100

    Use the latest milestone2 dmsg version
    Use streams instead of connections
    Different dial on server side
    Updated config setup for dmsg

--- transport.go
+++ transport.go
@@ -35,12 +35,6 @@ type DMSGTransport struct {
 // RoundTrip implements golang's http package support for alternative transport protocols.
 // In this case DMSG is used instead of TCP to initiate the communication with the server.
 func (t DMSGTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-
-	// connect to the DMSG server
-	if err := t.dmsgC.InitiateServerConnections(context.Background(), 1); err != nil {
-		log.Fatalf("Error initiating server connections by initiator: %v", err)
-	}
-
 	// process remote pub key and port from dmsg-addr request header
 	addrSplit := strings.Split(req.Host, ":")
 	if len(addrSplit) != 2 {
@@ -54,28 +48,29 @@ func (t DMSGTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	port := uint16(rPort)
 
 	var (
-		transport    *dmsg.Transport
-		transportErr error
+		stream    *dmsg.Stream
+		streamErr error
 	)
 	for i := uint8(0); i < t.RetryCount; i++ {
-		transport, transportErr = t.dmsgC.Dial(context.Background(), pk, port)
-		if transportErr != nil {
-			log.Println("Transport was not established, retrying...")
+		serverAddress := dmsg.Addr{PK: pk, Port: port}
+		stream, streamErr = t.dmsgC.DialStream(context.Background(), serverAddress)
+		if streamErr != nil {
+			log.Printf("Error dialing responder: %s. retrying...", streamErr)
 			// Adding this to make sure we have enough time for delegate servers to become available
 			time.Sleep(200 * time.Millisecond)
 			continue
 		}
-		transportErr = nil
+		streamErr = nil
 		break
 	}
-	if transportErr != nil {
-		return nil, transportErr
+	if streamErr != nil {
+		return nil, streamErr
 	}
-	defer transport.Close()
+	defer stream.Close()
 
-	if err := req.Write(transport); err != nil {
+	if err := req.Write(stream); err != nil {
 		return nil, err
 	}
 
-	return http.ReadResponse(bufio.NewReader(transport), req)
+	return http.ReadResponse(bufio.NewReader(stream), req)
 }
